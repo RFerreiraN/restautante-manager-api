b7fea6278bd6f55c1aac421d1a31d07fc004f1c5
RFerreiraN <ricardevferreira@gmail.com>
2026-03-02 22:01:47 +0100

    Nueva estructura del proyecto

---
+++ src/Models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  refreshToken: {
+    type: String
+  }
+})
+
+export const User = mongoose.model('user', UserSchema)
