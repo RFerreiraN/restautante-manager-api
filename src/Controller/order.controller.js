b262e4667d796fedabad6fdccfe019267e445da9
RFerreiraN <ricardevferreira@gmail.com>
2026-03-13 00:49:17 +0100

    avanzamos con el controller del modulo order

---
+++ src/Controller/order.controller.js
@@ -0,0 +1,58 @@
+import { OrderService } from '../Service/order.service'
+import { validateOrder, validatePartialOrder } from '../utils/Validations/order.validator.js'
+
+export class OrderController {
+  static async createOrder(req, res) {
+    const results = validateOrder(req.body)
+    if (!results.success) {
+      return res.status(400).json({ message: JSON.parse(results.error.message) })
+    }
+
+    const validateData = {
+      ...results.data,
+      user: req.user.id
+    }
+
+    try {
+      const order = await OrderService.createOrder(validateData)
+      return res.status(201).json(order)
+    } catch (error) {
+      return res.status(400).json({ message: error.message })
+    }
+  }
+
+  static async getAllOrders(req, res) {
+    try {
+      const orders = await OrderService.getAllOrders()
+      return res.status(200).json(orders)
+    } catch (error) {
+      return res.status(400).json({ message: error.message })
+    }
+  }
+
+  static async getOrderById(req, res) {
+    const { id } = req.params
+    try {
+      const order = await OrderService.getOrderById(id)
+      return res.status(200).json(order)
+    } catch (error) {
+      return res.status(404).json({ message: error.message })
+    }
+  }
+
+  static async updateStatus(req, res) {
+    const results = validatePartialOrder(req.body)
+    if (!results.success) {
+      return res.status(400).json({ message: JSON.parse(results.error.message) })
+    }
+    const { id } = req.params
+    const { status } = results.data
+
+    try {
+      const orderStatus = await OrderService.updateStatus(id, status)
+      return res.status(200).json(orderStatus)
+    } catch (error) {
+      return res.status(400).json({ message: error.message })
+    }
+  }
+}
--- src/utils/Validations/order.validator.js
+++ src/utils/Validations/order.validator.js
@@ -1,11 +1,11 @@
 import z from 'zod'
 
 const orderSchema = z.object({
-  user: z.string().min(1, { message: 'ID user required' }),
+  user: z.string().min(1, { message: 'ID user required' }).optional(),
   items: z.array(z.object({ product: z.string().min(1), quantity: z.number().int().positive().min(1) })).min(1),
   status: z.enum(['pending', 'preparing', 'ready', 'delivered', 'cancelled']).optional(),
   table: z.string().min(1),
-  total: z.number().positive()
+  total: z.number().positive().optional()
 })
 
 export function validateOrder(object) {
